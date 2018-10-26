322f8c4bf09db473ec1c38a7702dc8f6256580c4
Dan Pinto <fozzarelo@gmail.com>
2018-10-26 10:00:26 -0700

    lessons 31-55

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://fozzarelo:3l3fant3@ds141613.mlab.com:41613/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
